9b22f499a4c2ecdbba7dd3018516fc16bf9d3b1e
ssfang <fss@originqc.com>
2021-02-09 17:22:32 +0800

    Initial Projest

---
+++ static/main/js/index.js
@@ -0,0 +1,19 @@
+Mock.mock("/api/buyer",{
+    'name':'@NAME',
+    'color':'@COLOR',
+    'email':'@EMAIL',
+});
+
+$.ajax({
+    url: "/api/buyer",
+    type: "GET",
+    data: "",
+    success: function (data) {
+        console.log("+++++++++++++++===");
+        console.log(data);
+    },
+    error:function (error) {
+        console.log("======================");
+        console.log(error);
+    }
+});
